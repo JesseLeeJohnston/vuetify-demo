14d569e92210143f872ba572ff7f45b3f31f3bac
jesseleejohnston <jesseleejohnston@gmail.com>
2019-06-13 22:51:38 -0700

    First commit. Setup Vuetify project with vuerouter and page transitions.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,60 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Staff</span>
+        <span class="font-weight-light">EDITOR</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat to="/">
+        Home
+      </v-btn>
+      <v-btn flat to="about">
+        About
+      </v-btn>
+      <v-btn flat to="contact">
+        Contact
+      </v-btn>
+    </v-toolbar-items>
+      
+    </v-toolbar>
+
+    <v-content>
+      <v-container fluid>
+        <transition name="fade" mode="out-in">
+          <router-view></router-view>
+        </transition>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data () {
+    return {
+      //
     }
   }
+}
+</script>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
